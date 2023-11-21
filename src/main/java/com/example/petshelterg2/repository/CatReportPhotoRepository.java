18a9c26894a0f53b9da1186372a0708fcdc22137
Danila <03denchik03@gmail.com>
2023-11-21 14:08:45 +0300

    Поправил модели

---
+++ src/main/java/com/example/petshelterg2/repository/CatReportPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.example.petshelterg2.repository;
+
+import com.example.petshelterg2.model.CatReportPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatReportPhotoRepository extends JpaRepository<CatReportPhoto,Long> {
+}
